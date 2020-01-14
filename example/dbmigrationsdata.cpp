1acdd255bf49274a927a7816064c608f6be20a2a
Wojciech Knieć <wkniec@milosolutions.com>
2020-01-14 18:19:47 +0100

    refs #83515 add new migration example

--- example/dbmigrationsdata.cpp
+++ example/dbmigrationsdata.cpp
@@ -7,7 +7,7 @@
 #include "dbmigration.h"
 
 //!!! Always remember to update current version for any db changes!
-const QVersionNumber db::LATEST_DB_VERSION = { 0, 0, 2 };
+const QVersionNumber db::LATEST_DB_VERSION = { 0, 0, 3 };
 
 //!!! Add migrations here
 const QVector<db::Migration> db::DB_MIGRATIONS = {
@@ -36,4 +36,16 @@ const QVector<db::Migration> db::DB_MIGRATIONS = {
             QLatin1String("DROP TABLE `User`")
         })
     },
+
+    MigrationBuilder::builder()
+        .setVersion("0.0.3")
+        .addForwardQuery("CREATE TABLE `Fun`  ("
+                         "`id` INTEGER primary key UNIQUE,"
+                         "`name` TEXT NOT NULL,"
+                         "`type` INTEGER NOT NULL"
+                         ")")
+        .addForwardQuery("INSERT INTO `Fun`   (`name`, `type`)"
+                         "VALUES ('Super Fun', 0)")
+        .addBackwardQuery("DROP TABLE `Fun`")
+        .build()
 };
