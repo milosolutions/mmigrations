445e7ebe3f9499140720b7f7bd4db758359d827c
Wojciech Knieć <wkniec@milosolutions.com>
2020-01-22 09:04:11 +0100

    refs #83411 add sqlite cipher example

--- example/main.cpp
+++ example/main.cpp
@@ -12,8 +12,24 @@
 #include <QLoggingCategory>
 
 #include <connectionproviders/connectionprovidersqlite.h>
+#include <connectionproviders/connectionprovidersqlitecipher.h>
 
 Q_DECLARE_LOGGING_CATEGORY(migrations)
+
+namespace mdb = mdatabase;
+
+void printUsers(const QSqlDatabase& connection)
+{
+    auto query = QSqlQuery(connection);
+    query.prepare("SELECT * FROM `User`;");
+    mdb::Helpers::execQuery(query);
+
+    qCDebug(migrations) << "USERS:";
+    while (query.next()) {
+        auto userName = query.value(1).toString();
+        qCDebug(migrations) << userName;
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -23,13 +39,16 @@ int main(int argc, char *argv[])
     app.setApplicationName(AppName);
 
 
+    // **** SQLITE 3 ****
+    qCDebug(migrations) << "==================================";
+    qCDebug(migrations) << "SQLite3 example:";
     // setting up SQLite db path
-    mdatabase::ConnectionProviderSQLite::instance().setupConnectionData(
+    mdb::ConnectionProviderSQLite::instance().setupConnectionData(
                 QStandardPaths::writableLocation(
                     QStandardPaths::AppDataLocation) + "/local.db");
 
     // SQlite migrations manager 
-    using SqliteMigrations = mdatabase::MigrationManager<mdatabase::ConnectionProviderSQLite>;
+    using SqliteMigrations = mdb::MigrationManager<mdb::ConnectionProviderSQLite>;
     SqliteMigrations dbManager;
     QObject::connect(&dbManager, &SqliteMigrations::databaseUpdateStarted,
                      &app, []{ qInfo() << "Database update started!"; });
@@ -37,22 +56,41 @@ int main(int argc, char *argv[])
                      &app, []{ qInfo() << "Database update error!"; });
     QObject::connect(&dbManager, &SqliteMigrations::databaseReady,
                      &app, [&]{
-        qCInfo(migrations) << "Database ready!";
-
-        // example usage
-        auto dbConnection = dbManager.provider().databaseConnection();
-        auto query = QSqlQuery(dbConnection);
-        query.prepare("SELECT * FROM `User`;");
-        mdatabase::Helpers::execQuery(query);
-
-        qCDebug(migrations) << "USERS:";
-        while (query.next()) {
-            auto userName = query.value(1).toString();
-            qCDebug(migrations) << userName;
-        }
-    });
-
+                        qCInfo(migrations) << "Database ready!";
+                        printUsers(dbManager.provider().databaseConnection());
+                     });
     dbManager.setupDatabase();
+
+
+
+    // **** SQLITE 3 Cipher ****
+
+    qCDebug(migrations) << "==================================";
+    qCDebug(migrations) << "SQlite3Cipher example:";
+
+    auto& cipher= mdb::ConnectionProviderSQLiteCipher::instance();
+    if (cipher.checkPluginAvailable()) {
+        cipher.setPassword("secret");
+        cipher.setupConnectionData(QStandardPaths::writableLocation
+                               (QStandardPaths::AppDataLocation)+ "/localCipher.db");
+        using SqliteCipherMigrations = mdb::MigrationManager<mdb::ConnectionProviderSQLiteCipher>;
+        SqliteCipherMigrations cipherManager;
+        QObject::connect(&cipherManager, &SqliteCipherMigrations::databaseUpdateStarted,
+                         &app, []{ qInfo() << "Ciphered database update started!"; });
+        QObject::connect(&cipherManager, &SqliteCipherMigrations::databaseUpdateError,
+                         &app, []{ qInfo() << "Ciphered database update error!"; });
+        QObject::connect(&cipherManager, &SqliteCipherMigrations::databaseReady,
+                         &app, [&]{
+                            qCInfo(migrations) << "Ciphered database ready!";
+                            printUsers(cipherManager.provider().databaseConnection());
+                         });
+        cipherManager.setupDatabase();
+    } else {
+        qCCritical(migrations) << "SQLite3 Cipher Plugin was not found!!!"
+                               << "Check mmigraions/scripts/build_sqlitecipherplugin.sh "
+                                  "script for details";
+
+    }
 
     return app.exec();
 }
